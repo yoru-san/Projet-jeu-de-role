ab75586f712128587d97bfb66245d855f2954bb0
yoru-san <cecel31@free.fr>
2016-11-21 19:45:19 +0100

    optimisation en commentaire

--- js/artefact.js
+++ js/artefact.js
@@ -10,3 +10,16 @@ function Artefact (){
 
     }
 }
+
+// var Artefact = function(id, nom){
+//   this.id = id;
+//   this.nom = nom;
+//
+//   Artefact.prototype.use = function(){
+//     console.log(this.nom + " a été utilisé !");
+//   }
+// }
+//
+// var artefact = new Artefact(1, "Truc ");
+// artefact.use();
+// --> Truc a été utilisé
