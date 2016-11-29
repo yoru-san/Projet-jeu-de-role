7d1c6f1cbb754ff544c144084f492df47613ff2c
yoru-san <cecel31@free.fr>
2016-11-29 08:42:25 +0100

    correction/rajout de parenthèses

--- js/Character.class.js
+++ js/Character.class.js
@@ -1,20 +1,21 @@
-this.Character = function (game, map)
-{
-  // propriétés du pacman
+this.Character = function (game, map) {
+  // Propriétés du pacman
   var position  = null,
-      direction = null,
-      eaten     = null,
-      due       = null,
-      lives     = null,
-      score     = 5,
-      keyMap    = {};
+  direction = null,
+  eaten     = null,
+  due       = null,
+  lives     = null,
+  score     = 5,
+  keyMap    = {};
+}
 
-  // le déplacement en fonction des touches directionnelles
-  keyMap[KEY.ARROW_LEFT]  = LEFT;
-  keyMap[KEY.ARROW_UP]    = UP;
-  keyMap[KEY.ARROW_RIGHT] = RIGHT;
-  keyMap[KEY.ARROW_DOWN]  = DOWN;
+// le déplacement en fonction des touches directionnelles
+keyMap[KEY.ARROW_LEFT]  = LEFT;
+keyMap[KEY.ARROW_UP]    = UP;
+keyMap[KEY.ARROW_RIGHT] = RIGHT;
+keyMap[KEY.ARROW_DOWN]  = DOWN;
 
-  this.setPosition = function(x_pos, y_pos){
-    this.x = x_pos;
-    this.y = y_pos;
+this.setPosition = function(x_pos, y_pos) {
+  this.x = x_pos;
+  this.y = y_pos;
+}
