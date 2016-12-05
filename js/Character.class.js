80abf2fe5bdaba3a97c3aacd94a33411bf59ea3d
yoru-san <cecel31@free.fr>
2016-12-05 14:10:22 +0100

    modification classe character

--- js/Character.class.js
+++ js/Character.class.js
@@ -1,21 +1,27 @@
-this.Character = function (game, map) {
+function Character(game, map) {
   // Propriétés du pacman
-  var position  = null,
-  direction = null,
-  eaten     = null,
-  due       = null,
-  lives     = null,
-  score     = 5,
-  keyMap    = {};
-}
+  this.position = {},
+  this.direction = {
+    LEFT: false,
+    UP: false,
+    RIGHT: false,
+    DOWN: false
+  },
+  this.eaten = false,
+  this.due = null,
+  this.lives = 3,
+  this.score = 5,
 
-// le déplacement en fonction des touches directionnelles
-keyMap[KEY.ARROW_LEFT]  = LEFT;
-keyMap[KEY.ARROW_UP]    = UP;
-keyMap[KEY.ARROW_RIGHT] = RIGHT;
-keyMap[KEY.ARROW_DOWN]  = DOWN;
+  // le déplacement en fonction des touches directionnelles
+  this.keyMap = {
+    ARROW_LEFT: false,
+    ARROW_UP: false,
+    ARROW_RIGHT: false,
+    ARROW_DOWN: false
+  };
 
-this.setPosition = function(x_pos, y_pos) {
-  this.x = x_pos;
-  this.y = y_pos;
+  this.setPosition = function(x_pos, y_pos) {
+    this.position.x = x_pos;
+    this.position.y = y_pos;
+  }
 }
