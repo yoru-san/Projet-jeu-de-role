d1bef9d6cf8818c2b0bb4585c3b96b6285c76fc9
braxyxx <braxyxx31@gmail.com>
2016-12-05 14:52:45 +0100

    Add wall et getIdFromPosition

--- js/Cell.class.js
+++ js/Cell.class.js
@@ -1,8 +1,17 @@
 function Cell(x_pos, y_pos){
-    
+
     this.x = x_pos;
     this.y = y_pos;
     this.id = "c"+this.x+"-"+this.y;
     this.patGum = false;
-    
+    this.wall = false;
+}
+
+function addwall(x,y) {
+    return cell.wall(x,y) = true;
+}
+
+function getIdFromPosition(x,y) {
+  cell.x(x,y) = x;
+  cell.y(x,y) = y;
 }
